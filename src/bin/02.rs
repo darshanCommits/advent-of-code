9511c29e8a4cc0a0025ecdfe5c679733f337272a
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2024-04-22 23:41:59 +0530

    Day2 Part2
    I wasted a lot of time solving the wrong question.
    (misread the question statement oof)

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use itertools::Itertools;
 use std::cmp::Ordering;
 
@@ -45,17 +46,44 @@ impl Move {
             Move::Scissor => 3,
         }
     }
+
+    fn win(self) -> Self {
+        match self {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissor,
+            Move::Scissor => Move::Rock,
+        }
+    }
+
+    fn lose(self) -> Self {
+        match self {
+            Move::Rock => Move::Scissor,
+            Move::Paper => Move::Rock,
+            Move::Scissor => Move::Paper,
+        }
+    }
+
+    fn fight2(self, other: char) -> u32 {
+        match other {
+            // x is lose, y is draw, z is win
+            'X' => self.lose().to_num(),
+            'Y' => 3 + self.to_num(),
+            'Z' => 6 + self.win().to_num(),
+            _ => panic!("Invalid"),
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Round(Move, Move);
 
+// works only for 1 it will complicate things a lot if i introduce another enum to handle the part2 this way.
 impl Round {
     fn new(x: (Move, Move)) -> Self {
         Round(x.0, x.1)
     }
 
-    fn total_score(self) -> u32 {
+    fn score(self) -> u32 {
         match self.0.partial_cmp(&self.1) {
             Some(Ordering::Equal) => 3 + self.1.to_num(),
             Some(Ordering::Less) => 6 + self.1.to_num(),
@@ -73,14 +101,27 @@ pub fn part_one(input: &str) -> Option<u32> {
         .filter_map(Move::from)
         .tuples()
         .map(Round::new)
-        .map(Round::total_score)
+        .map(Round::score)
         .sum();
 
     Some(res)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let res: u32 = input
+        .lines()
+        .map(|line| {
+            let moves: Vec<char> = line
+                .chars()
+                .filter(|&c| !c.is_whitespace())
+                .collect();
+
+            let opp_mv = Move::from(moves[0]).unwrap();
+            opp_mv.fight2(moves[1])
+        })
+        .sum();
+
+    Some(res)
 }
 
 #[cfg(test)]
@@ -91,14 +132,13 @@ mod tests {
     fn test_part_one() {
         let result =
             part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(45));
+        assert_eq!(result, Some(15));
     }
 
     #[test]
-    #[ignore]
     fn test_part_two() {
         let result =
             part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(12));
     }
 }
